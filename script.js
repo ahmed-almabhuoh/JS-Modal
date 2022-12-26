c24c739c3c0fe79db4d65ffc0f20e4f518d30cbc
Ahmad Almabhouh <az54546@gmail.com>
2022-12-26 11:06:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpenModals = document.querySelectorAll('.show-modal');
+const btnCloseModals = document.querySelector('.close-modal');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnOpenModals.forEach(btn => btn.addEventListener('click', openModal));
+btnCloseModals.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
